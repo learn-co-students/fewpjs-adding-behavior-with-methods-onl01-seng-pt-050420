9b23b1244a2ea8c7eea2688a5be8436c8229653b
Pwako11 <akolo@hotmail.com>
2021-01-24 03:02:06 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+class Cat{
+    constructor(name){
+        this.name = name
+    }
+
+    speak(){
+        console.log(`${this.name} says meow!`);
+    }
+}
+
+class Dog{
+    constructor(name){
+        this.name = name
+    }
+
+    speak(){
+        console.log(`${this.name} says woof!`)
+    }
+}
+
+class Bird{
+    constructor(name){
+        this.name = name
+    }
+
+    speak(){
+        console.log(`It\'s me! ${this.name}, the parrot`)
+    }
+}
